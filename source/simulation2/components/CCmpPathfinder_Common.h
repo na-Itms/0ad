5e8eba4870b356aea6801dcc502702c490bfb5d2
na-Itms <na.itms76@gmail.com>
2015-05-07 22:22:20 +0200

    Fix another stupid merge problem

--- source/simulation2/components/CCmpPathfinder_Common.h
+++ source/simulation2/components/CCmpPathfinder_Common.h
@@ -200,8 +200,6 @@ public:
 	 */
 	virtual void UpdateGrid();
 
-	Grid<u16> ComputeShoreGrid();
-
 	void ComputeTerrainPassabilityGrid(const Grid<u16>& shoreGrid);
 
 	void RenderSubmit(SceneCollector& collector);
