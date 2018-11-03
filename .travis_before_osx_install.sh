6cd1ee17cbf611647b751b1d6fd3aa9e8633d22d
na-Itms <na.itms76@gmail.com>
2018-11-03 20:17:48 +0100

    add Travis CI testing

---
+++ .travis_before_osx_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+brew update
+brew outdated cmake || brew upgrade cmake
+brew outdated pkgconfig || brew upgrade pkgconfig
+
+cd libraries/osx
+# Travis will terminate the build if the log exceeds 4MB
+# so we'll send stdout to /dev/null.
+#
+./build-osx-libs.sh > /dev/null
+
